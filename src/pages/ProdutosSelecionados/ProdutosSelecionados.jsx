41b239d0227e97b638cbd5d321b3c96b3e39fce1
Anderson Tome <anderson_tome@outlook.com.br>
2022-08-28 20:32:58 -0300

    Exercicio 9

--- src/pages/ProdutosSelecionados/ProdutosSelecionados.jsx
+++ src/pages/ProdutosSelecionados/ProdutosSelecionados.jsx
@@ -5,9 +5,14 @@ import { ListaCards } from "../../components/ListaCards/ListaCards.jsx";
 export const ProdutosSelecionados = () => {
   const { produtosSelecionados } = useProdutosSelecionados();
 
+  const somaProdutos = produtosSelecionados.reduce((valorInicial, proximoValor) => {
+    return valorInicial + proximoValor.valor
+  }, 0)
+
   return (
     <>
       <h1>Produtos Selecionados</h1>
+      <p>Total dos Produtos: R$ {somaProdutos}</p>
       <ListaCards produtos={produtosSelecionados} />
     </>
   )
