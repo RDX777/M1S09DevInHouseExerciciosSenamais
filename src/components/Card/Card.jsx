aa392f2c49e9563f8e8b9e0e4dd2ac90befc4f92
Anderson Tome <anderson_tome@outlook.com.br>
2022-08-28 14:34:17 -0300

    Exrcicio 4

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,28 +2,36 @@ import PropTypes from 'prop-types';
 
 import styles from './Card.module.css';
 
-export const Card = ({ produto, selecionado, onSelecionar }) => (
-  <button
-    onClick={() => onSelecionar(produto.id)}
-    className={`${styles.card} ${selecionado ? styles.cardSelecionado : ''}`}
-  >
-    <img className={styles.img} alt='Foto do prato' src={produto.img} height={200} />
+import { useProdutosSelecionados } from '../../contexto/ProdutosSelecionados/useProdutosSelecionados.jsx';
 
-    <div className={styles.info}>
-      <h2 className={styles.infoTitulo}>{produto.nome}</h2>
+export const Card = ({ produto }) => {
 
-      <p className={styles.infoDescricao}>{produto.descricao}</p>
-    </div>
+  const { isSelecionado, handleSelecionar } = useProdutosSelecionados()
+  return (
+    <>
+      <button
+        onClick={() => {handleSelecionar(produto)}}
+        className={`${styles.card} ${isSelecionado(produto.id) ? styles.cardSelecionado : ''}`}
+      >
+        <img className={styles.img} alt='Foto do prato' src={produto.img} height={200} />
 
-    <div className={styles.footer}>
-      <strong className={styles.footerValor}>
-        {`R$ ${produto.valor.toFixed(2).replace('.', `,`)}`}
-      </strong>
+        <div className={styles.info}>
+          <h2 className={styles.infoTitulo}>{produto.nome}</h2>
 
-      <p>{produto.tempoPreparo}</p>
-    </div>
-  </button>
-);
+          <p className={styles.infoDescricao}>{produto.descricao}</p>
+        </div>
+
+        <div className={styles.footer}>
+          <strong className={styles.footerValor}>
+            {`R$ ${produto.valor.toFixed(2).replace('.', `,`)}`}
+          </strong>
+
+          <p>{produto.tempoPreparo}</p>
+        </div>
+      </button>
+    </>
+  )
+};
 
 Card.propTypes = {
   produto: PropTypes.shape({
--- src/components/ListaCards/ListaCards.jsx
+++ src/components/ListaCards/ListaCards.jsx
@@ -4,11 +4,7 @@ import { Card } from '@components';
 
 import styles from './ListaCards.module.css';
 
-import { useProdutosSelecionados } from '../../contexto/ProdutosSelecionados/useProdutosSelecionados.jsx';
-
 export const ListaCards = ({ produtos }) => {
-
-  const { isSelecionado, handleSelecionar } = useProdutosSelecionados()
 
   return (
     <ul className={styles.lista}>
@@ -16,8 +12,6 @@ export const ListaCards = ({ produtos }) => {
         <li className={styles.listaItem} key={produto.id}>
           <Card
             produto={produto}
-            selecionado={isSelecionado(produto.id)}
-            onSelecionar={() => {handleSelecionar(produto)}}
           />
         </li>
       ))}
